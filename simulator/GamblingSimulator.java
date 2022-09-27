18d001f407f88e5ead82c0517a1d34e90a7d21f4
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-27 17:09:22 +0530

    Modify:corrected logic for profit and loss

--- simulator/GamblingSimulator.java
+++ simulator/GamblingSimulator.java
@@ -3,32 +3,33 @@ package gambling.simulator;
 class Gambler {
 	int stake;
 	int profit;
-	int loose;
-
+	
 	Gambler() {
 		this.stake = 100;
 		this.profit = 0;
-		loose = 0;
 	}
 
 	// start playing
-	//bet for either win or loose
+	// bet for either win or loose
 	void play() {
-		int outcome = (int) (Math.floor(Math.random() * 10) % 2);
-		switch (outcome) {
-		case 0:
-			this.stake -= 1;
-			this.loose -= 1;
-			System.out.println("You loose this game !  ");
-			System.out.println("loose is " + this.loose);
-			break;
-		case 1:
-			this.stake += 1;
-			this.profit += 1;
-			System.out.println("You can start the game \nBut you can invest maximum 100$ in a day \nAnd bet $1 everygame");		
-			System.out.println("profit is " + this.profit);
-			break;
-		}
+		//while (this.stake < 150 && this.stake > -50) {
+			int outcome = (int) (Math.floor(Math.random() * 10) % 2);
+			switch (outcome) {
+			case 0:
+				this.stake -= 1;
+				this.profit -= 1;
+				System.out.println("You loose this game !  ");
+				System.out.println("loss is  "+this.profit);
+				break;
+			case 1:
+				this.stake += 1;
+				this.profit += 1;
+				System.out.println("You win this game !");
+				System.out.println("profit is  "+this.profit);		
+				break;
+			}
+		//}
+	//	System.out.println("profit"+this.stake);
 	}
 }
 
