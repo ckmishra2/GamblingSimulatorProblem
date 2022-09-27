96109cc4af03ea11dfb2d04abd4d7b460e41e7af
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-27 17:40:39 +0530

    Add:After 20 days playing every day and know the total won and loose

--- simulator/GamblingSimulator.java
+++ simulator/GamblingSimulator.java
@@ -3,40 +3,61 @@ package gambling.simulator;
 class Gambler {
 	int stake;
 	int profit;
+	int countWin, countLoss;
 
 	Gambler() {
 		this.stake = 100;
 		this.profit = 0;
+		this.countLoss=0;
+		this.countWin=0;
+		
 	}
 
 	// start playing
 	// bet for either win or loose
 	// As a gambler if won or lost 50% of the stake,would resign for the day
 	void play() {
+		int daysProfit=0;
+		this.stake=100;
 		while (this.stake < 150 && this.stake > 50) {
 			int outcome = (int) (Math.floor(Math.random() * 10) % 2);
 			switch (outcome) {
 			case 0:
 				this.stake -= 1;
 				this.profit -= 1;
+				daysProfit-=1;
 				System.out.println("You loose this game !  ");
-				System.out.println("loss is  " + this.profit);
 				break;
 			case 1:
 				this.stake += 1;
 				this.profit += 1;
+				daysProfit+=1;
 				System.out.println("You win this game !");
-				System.out.println("profit is  " + this.profit);
 				break;
 			}
 		}
 		System.out.println("Gambler is resigning for the day \nToday stake is "+ this.stake);
+		if(daysProfit>0)
+		{
+			this.countWin++;
+			System.out.println("Todays profit is "+ daysProfit);
+		}
+		if(daysProfit<0)
+		{
+			this.countLoss++;
+			System.out.println("Today loss is "+daysProfit);
+		}
 	}
 }
 
 public class GamblingSimulator {
 	public static void main(String[] args) {
 		Gambler player = new Gambler();
-		player.play();
+		//play for 20 days
+		for(int day=0;day<20;day++)
+		{
+			player.play();
+		}
+		System.out.println("win "+player.countWin+"\nloss "+player.countLoss);
 	}
 }
