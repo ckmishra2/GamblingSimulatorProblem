bfb3df8eb167c5ef3be0de5859b5ed409e5b8035
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-27 17:16:29 +0530

    Add:Gambler is resign after win or loose the game

--- simulator/GamblingSimulator.java
+++ simulator/GamblingSimulator.java
@@ -3,7 +3,7 @@ package gambling.simulator;
 class Gambler {
 	int stake;
 	int profit;
-	
+
 	Gambler() {
 		this.stake = 100;
 		this.profit = 0;
@@ -11,25 +11,26 @@ class Gambler {
 
 	// start playing
 	// bet for either win or loose
+	// As a gambler if won or lost 50% of the stake,would resign for the day
 	void play() {
-		//while (this.stake < 150 && this.stake > -50) {
+		while (this.stake < 150 && this.stake > 50) {
 			int outcome = (int) (Math.floor(Math.random() * 10) % 2);
 			switch (outcome) {
 			case 0:
 				this.stake -= 1;
 				this.profit -= 1;
 				System.out.println("You loose this game !  ");
-				System.out.println("loss is  "+this.profit);
+				System.out.println("loss is  " + this.profit);
 				break;
 			case 1:
 				this.stake += 1;
 				this.profit += 1;
 				System.out.println("You win this game !");
-				System.out.println("profit is  "+this.profit);		
+				System.out.println("profit is  " + this.profit);
 				break;
 			}
-		//}
-	//	System.out.println("profit"+this.stake);
+		}
+		System.out.println("Gambler is resigning for the day \nToday stake is "+ this.stake);
 	}
 }
 
